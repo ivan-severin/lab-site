03019a24c5dbb99a2dfd0c3f89e3d60ba6b38b9a
ivan-severin <ivan.severin.m@gmail.com>
2017-08-05 03:08:26 +0300

    fixed models.py and views.py

--- app/models.py
+++ app/models.py
@@ -30,6 +30,31 @@ class Entry(flask_db.Model):
             force_insert = False
         fts_entry.content = '\n'.join((self.title, self.content))
         fts_entry.save(force_insert=force_insert)
+    
+    @classmethod
+    def public(cls):
+        return Entry.select().where(Entry.published == True)
+
+    @classmethod
+    def search(cls, query):
+        words = [word.strip() for word in query.split() if word.strip()]
+        if not words:
+            # Return empty query.
+            return Entry.select().where(Entry.id == 0)
+        else:
+            search = ' '.join(words)
+
+        return (FTSEntry
+                .select(
+                    FTSEntry,
+                    Entry,
+                    FTSEntry.rank().alias('score'))
+                .join(Entry, on=(FTSEntry.entry_id == Entry.id).alias('entry'))
+                .where(
+                    (Entry.published == True) &
+                    (FTSEntry.match(search)))
+                .order_by(SQL('score').desc()))
+
 
 
 class FTSEntry(FTSModel):
--- app/views.py
+++ app/views.py
@@ -1,6 +1,133 @@
-from app import app
+from app import app, request, object_list, Response, session, get_object_or_404
+from app.models import Entry
+
+import functools
+
+
+# @app.route('/index')
+# def index():
+# 	return 'Hello World! :)'
+
+
+def login_required(fn):
+    @functools.wraps(fn)
+    def inner(*args, **kwargs):
+        if session.get('logged_in'):
+            return fn(*args, **kwargs)
+        return redirect(url_for('login', next=request.path))
+    return inner
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    next_url = request.args.get('next') or request.form.get('next')
+    if request.method == 'POST' and request.form.get('password'):
+        password = request.form.get('password')
+        if password == app.config['ADMIN_PASSWORD']:
+            session['logged_in'] = True
+            session.permanent = True  # Use cookie to store session.
+            flash('You are now logged in.', 'success')
+            return redirect(next_url or url_for('index'))
+        else:
+            flash('Incorrect password.', 'danger')
+    return render_template('login.html', next_url=next_url)
+
+@app.route('/logout/', methods=['GET', 'POST'])
+def logout():
+    if request.method == 'POST':
+        session.clear()
+        return redirect(url_for('login'))
+    return render_template('logout.html')
+
+
+
+
+
 
 @app.route('/')
-@app.route('/index')
 def index():
-	return 'Hello World! :)'
+    search_query = request.args.get('q')
+    if search_query:
+        query = Entry.search(search_query)
+    else:
+        query = Entry.public().order_by(Entry.timestamp.desc())
+    return object_list('index.html', query, search=search_query)
+
+
+@app.route('/<slug>/')
+def detail(slug):
+    if session.get('logged_in'):
+        query = Entry.select()
+    else:
+        query = Entry.public()
+    entry = get_object_or_404(query, Entry.slug == slug)
+    return render_template('detail.html', entry=entry)
+
+
+@property
+def html_content(self):
+    hilite = CodeHiliteExtension(linenums=False, css_class='highlight')
+    extras = ExtraExtension()
+    markdown_content = markdown(self.content, extensions=[hilite, extras])
+    oembed_content = parse_html(
+        markdown_content,
+        oembed_providers,
+        urlize_all=True,
+        maxwidth=app.config['SITE_WIDTH'])
+    return Markup(oembed_content)
+
+
+
+@app.route('/create/', methods=['GET', 'POST'])
+@login_required
+def create():
+    if request.method == 'POST':
+        if request.form.get('title') and request.form.get('content'):
+            entry = Entry.create(
+                title=request.form['title'],
+                content=request.form['content'],
+                published=request.form.get('published') or False)
+            flash('Entry created successfully.', 'success')
+            if entry.published:
+                return redirect(url_for('detail', slug=entry.slug))
+            else:
+                return redirect(url_for('edit', slug=entry.slug))
+        else:
+            flash('Title and Content are required.', 'danger')
+    return render_template('create.html')
+
+
+@app.route('/<slug>/edit/', methods=['GET', 'POST'])
+@login_required
+def edit(slug):
+    entry = get_object_or_404(Entry, Entry.slug == slug)
+    if request.method == 'POST':
+        if request.form.get('title') and request.form.get('content'):
+            entry.title = request.form['title']
+            entry.content = request.form['content']
+            entry.published = request.form.get('published') or False
+            entry.save()
+
+            flash('Entry saved successfully.', 'success')
+            if entry.published:
+                return redirect(url_for('detail', slug=entry.slug))
+            else:
+                return redirect(url_for('edit', slug=entry.slug))
+        else:
+            flash('Title and Content are required.', 'danger')
+
+    return render_template('edit.html', entry=entry)
+
+
+
+
+@app.template_filter('clean_querystring')
+def clean_querystring(request_args, *keys_to_remove, **new_values):
+    querystring = dict((key, value) for key, value in request_args.items())
+    for key in keys_to_remove:
+        querystring.pop(key, None)
+    querystring.update(new_values)
+    return urllib.urlencode(querystring)
+
+@app.errorhandler(404)
+def not_found(exc):
+    return Response('<h3>Not found</h3>'), 404
